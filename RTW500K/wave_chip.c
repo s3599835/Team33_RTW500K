b30e8dca3b9b7086f18d1aba0a5996d2b839f63f
Liam O'Shea <s3599835@student.rmit.edu.au>
2020-09-03 16:08:16 +1000

    Added phase, freq and mode set funcs, init routine for 9833

--- RTW500K/wave_chip.c
+++ RTW500K/wave_chip.c
@@ -18,4 +18,119 @@ int writeToAD9833 (OutputWaveForm * waveform)
 	//Liam
 	
 	return 0;
+<<<<<<< Updated upstream
+=======
+}
+
+void sendCommandAD9833(unsigned char byte1, unsigned char byte2)
+{
+	PORTB &= ~(1<<CS_AD9833);
+	SPDR = byte1;
+	while(!(SPSR & (1<<SPIF)));
+	
+	SPDR = byte2;
+	while(!(SPSR & (1<<SPIF)));
+	PORTB |= 1<<CS_AD9833;
+	
+}
+
+void initAD9833()
+{
+	unsigned char upperByte = 0x00;
+	unsigned char lowerByte = 0x00;
+	
+	// Set reset and consecutive freq. write bits
+	setRegBit(&upperByte, AD_RESET);
+	setRegBit(&upperByte, AD_B28);
+	sendCommandAD9833(upperByte, lowerByte);
+	
+	// Initialize frequency and phase registers to known vals
+	setFrequency(AD_DEFAULT_FREQ);
+	setPhase(AD_DEFAULT_PHASE);
+	setMode(SINE);
+	
+	// Clear reset
+	clearRegBit(&upperByte, AD_RESET);
+	sendCommandAD9833(upperByte, lowerByte);
+	
+	
+}
+
+void setFrequency(int frequency)
+{
+	unsigned char firstUpperByte = 0x40;
+	unsigned char secondUpperByte = 0x40;
+	unsigned char firstLowerByte = 0x00;
+	unsigned char secondLowerByte = 0x00;
+	
+	uint32_t freqreg = (frequency * AD_2POW28/AD_MCLK);
+	
+	firstLowerByte = freqreg;
+	firstUpperByte |= ((freqreg >> 8) & 0x3f);
+	secondLowerByte = freqreg >> 16;
+	secondUpperByte |= ((freqreg >> 24) & 0x3f);
+	
+	sendCommandAD9833(firstUpperByte, firstLowerByte);
+	sendCommandAD9833(secondUpperByte, secondLowerByte);
+}
+
+// phase in degrees
+void setPhase(int phase)
+{
+	unsigned char upperByte = 0xC0;
+	unsigned char lowerByte = 0x00;
+	
+	uint16_t phasereg = (phase * 4096/(2*M_PI));
+	
+	lowerByte = phasereg;
+	upperByte |= ((phasereg >> 8) & 0x0f);
+	
+	sendCommandAD9833(upperByte, lowerByte);
+}
+
+void setMode(WaveType mode)
+{
+	unsigned char upperByte = 0x00;
+	unsigned char lowerByte = 0x00;
+	
+	switch(mode)
+	{
+		case SINE:
+			clearRegBit(lowerByte, AD_OPBITEN);
+			clearRegBit(lowerByte, AD_MODE);
+		break;
+		case SQURE:
+			setRegBit(lowerByte, AD_OPBITEN);
+			clearRegBit(lowerByte, AD_MODE);
+		break;
+		case TRIANGLE:
+			clearRegBit(lowerByte, AD_OPBITEN);
+			setRegBit(lowerByte, AD_MODE);
+		break;
+	}
+	
+	sendCommandAD9833(upperByte, lowerByte);
+}
+
+void setRegBit(unsigned char *byte, int bit)
+{
+	if (bit > 7)
+	{
+		*byte |= 1 << (bit-8);
+	} else
+	{
+		*byte |= 1 << bit;
+	}
+}
+
+void clearRegBit(unsigned char *byte, int bit)
+{
+	if (bit > 7)
+	{
+		*byte &= ~(1 << (bit-8));
+	} else
+	{
+		*byte &= ~(1 << bit);
+	}
+>>>>>>> Stashed changes
 }
--- RTW500K/wave_chip.h
+++ RTW500K/wave_chip.h
@@ -6,9 +6,35 @@
 #ifndef WAVE_CHIP_H
 #define WAVE_CHIP_H
 
+// Upper byte (AD_X-8)
+#define AD_B28		13
+#define AD_HLB		12
+#define AD_FSELECT	11
+#define AD_PSELECT	10
+#define AD_RESET	8
+
+// Lower byte (AD_X)
+#define AD_SLEEP1	7
+#define AD_SLEEP12	6
+#define AD_OPBITEN	5
+#define AD_DIV2		3
+#define AD_MODE		1
+
+#define AD_2POW28	(1 << 28)
+#define AD_MCLK		25000000
+#define AD_DEFAULT_FREQ 1000
+#define AD_DEFAULT_PHASE 0
+
 #include "utility.h"
 
 int writeSPI(int chipselect);
 int writeToAD9833(OutputWaveForm * waveform);
+<<<<<<< Updated upstream
+=======
+void sendCommandAD9833(unsigned char byte1, unsigned char byte2);
+void initAD9833();
+void setRegBit(unsigned char *byte, int bit);
+void clearRegBit(unsigned char *byte, int bit);
+>>>>>>> Stashed changes
 
 #endif
